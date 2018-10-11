f65b09839841a5db5f1a788167d97b8129a2e1d8
Haley Townsend <htownsen@andrew.cmu.edu>
2018-10-11 09:51:16 -0400

    Shapes are appearing on leaflet!! But only about half... keep getting error to install xml2 package. Will try

--- proj2app/app.R
+++ proj2app/app.R
@@ -77,20 +77,19 @@ ui <- fluidPage(theme = shinytheme("flatly"),
 server <- function(input, output) {
   pghLoad <- reactive({
     # Build URL Query
-    url <- URLencode(paste0("https://services1.arcgis.com/YZCmUqbcsUpOKfj7/arcgis/rest/services/PGHWebNeighborhoods/FeatureServer/0/query?where=HOOD+IS+NOT+NULL+AND+HOOD+%3D+%27", input$hoodSelect,
+    url <- URLencode(paste0("https://services1.arcgis.com/YZCmUqbcsUpOKfj7/arcgis/rest/services/PGHWebNeighborhoods/FeatureServer/0/query?where=HOOD+%3D+%27", input$hoodSelect,
                             "%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&returnCentroid=false&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=json"))
     # Get Data
-    sp <- getEsri(url) 
-    #%>% spTransform("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+    hoodpolys <- getEsri(url) %>% spTransform("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
 
-    return(sp)
+    return(hoodpolys)
   })
   output$map <- renderLeaflet({
-    sp <- pghLoad()
+    hoodpolys <- pghLoad()
     # Call Data and Build Map
     leaflet() %>%
       addTiles() %>%
-      addPolygons(data = sp) #, popup = ~paste0("<b>", Name, "</b><br>", addrln1, "<br>", hours, "<br>", phones, "<br>", description))
+      addPolygons(data = hoodpolys) #, popup = ~paste0("<b>", Name, "</b><br>", addrln1, "<br>", hours, "<br>", phones, "<br>", description))
   })
 }
 
