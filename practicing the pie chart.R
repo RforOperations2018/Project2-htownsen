148867dc215443ad83bf6d2e6b86e291cf5adbc2
Haley Townsend <htownsen@andrew.cmu.edu>
2018-10-16 09:44:22 -0400

    finally got the pie chart to work in isolation of shiny

--- practicing the pie chart.R
+++ practicing the pie chart.R
@@ -15,10 +15,33 @@ library(xml2)
 
 transpo = read.csv("transportation.csv")
 tshady = transpo[transpo$Neighborhood=='Shadyside',]
+
+# v <- loaddf()
+# t <- table(v$ZipCode)
+# w <- as.data.frame(t)
+# names(w)[1] <- "word"
+# names(w)[2] <- "freq"
+
+# class = factor
+
+tshady$Commute.to.Work..Drive.Alone..2010. <-  as.numeric(as.character(gsub('%', '', tshady$Commute.to.Work..Drive.Alone..2010.)))
+tshady$Commute.to.Work..Public.Transportation..2010. <-  as.numeric(as.character(gsub('%', '', tshady$Commute.to.Work..Public.Transportation..2010.)))
+tshady$Commute.to.Work..Motorcycle..2010. <-  as.numeric(as.character(gsub('%', '', tshady$Commute.to.Work..Motorcycle..2010)))
+tshady$Commute.to.Work..Walk..2010. <-  as.numeric(as.character(gsub('%', '', tshady$Commute.to.Work..Walk..2010)))
+tshady$Commute.to.Work..Carpool.Vanpool..2010. <-  as.numeric(as.character(gsub('%', '', tshady$Commute.to.Work..Carpool.Vanpool..2010)))
+tshady$Commute.to.Work..Taxi..2010. <-  as.numeric(as.character(gsub('%', '', tshady$Commute.to.Work..Taxi..2010.)))
+tshady$Commute.to.Work..Bicycle..2010. <-  as.numeric(as.character(gsub('%', '', tshady$Commute.to.Work..Bicycle..2010.)))
+tshady$Commute.to.Work..Other..2010. <-  as.numeric(as.character(gsub('%', '', tshady$Commute.to.Work..Other..2010.)))
+tshady$Work.at.Home..2010. <-  as.numeric(as.character(gsub('%', '', tshady$Work.at.Home..2010.)))
+
+
 tshady2 = tshady[,c("Commute.to.Work..Drive.Alone..2010.",	"Commute.to.Work..Public.Transportation..2010.", "Commute.to.Work..Motorcycle..2010.",
-                         "Commute.to.Work..Walk..2010.","Work.at.Home..2010.",	"Commute.to.Work..Carpool.Vanpool..2010.",	
-                         "Commute.to.Work..Taxi..2010.","Commute.to.Work..Bicycle..2010.","Commute.to.Work..Other..2010.")]
+                    "Commute.to.Work..Walk..2010.","Work.at.Home..2010.",	"Commute.to.Work..Carpool.Vanpool..2010.",
+                    "Commute.to.Work..Taxi..2010.","Commute.to.Work..Bicycle..2010.","Commute.to.Work..Other..2010.")]
 
-vals = tshady2[2,]
 
-plot_ly(tshady2, labels = colnames(tshady2), values = vals, type = 'pie')
+plot_ly(tshady2, labels = colnames(tshady2), values = as.numeric(tshady2[1,]) , type = 'pie')
+
+# Test case
+per = '44.80%'
+as.numeric(as.character(gsub('%', '', per)))
